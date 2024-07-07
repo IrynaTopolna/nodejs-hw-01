b324114d3a07af31b26ce2db0c33297f5a764c3b
IrynaTopolna <itopolna2808@gmail.com>
2024-07-07 14:08:41 +0300

    Adds funcs generate and addOne logic

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,28 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  const contact = createFakeContact();
+  console.log(contact);
+
+  const savedContacts = await getSavedContacts();
+  console.log(savedContacts);
+  const allContacts = [...savedContacts, contact];
+  console.log(allContacts);
+
+  await updateContacts(allContacts);
+};
+
+async function getSavedContacts() {
+  const savedContacts = await fs.readFile(PATH_DB);
+  return JSON.parse(savedContacts);
+}
+
+const updateContacts = (contacts) =>
+  fs
+    .writeFile(PATH_DB, JSON.stringify(contacts))
+    .then(() => console.log('Added'))
+    .catch((err) => console.error(err));
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,29 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const newContacts = [];
+  for (let i = 0; i < number; i++) {
+    const contact = createFakeContact();
+    newContacts.push(contact);
+  }
+
+  const savedContacts = await getSavedContacts();
+  const allContacts = savedContacts.concat(newContacts);
+
+  updateContacts(allContacts);
+};
+
+async function getSavedContacts() {
+  const savedContacts = await fs.readFile(PATH_DB);
+  return JSON.parse(savedContacts);
+}
+
+const updateContacts = (contacts) =>
+  fs
+    .writeFile(PATH_DB, JSON.stringify(contacts))
+    .then(() => console.log('Added'))
+    .catch((err) => console.error(err));
 
 generateContacts(5);
